101e4d461e6ab10b8d1488f3b8ec67cf3e29011c
Adrian Polo <a.whole.dev@gmail.com>
2015-09-19 01:30:53 +0200

    Modificado el node api

--- 15_nodeApi/app.js
+++ 15_nodeApi/app.js
@@ -20,6 +20,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+// Carpetas de contenido estatico
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
--- 15_nodeApi/routes/users.js
+++ 15_nodeApi/routes/users.js
@@ -6,4 +6,31 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/:id', function(req, res, next) {
+    //Aqui aparecen los parametros que nos han pasado por req.
+    console.log(req.params);
+
+    var id = req.params.id;
+
+    res.send('respond with a resource');
+});
+
+// El next lo podemos quitar si no lo empleamos
+router.get('/:coche', function(req, res, next) {
+    //Aqui aparecen los parametros que nos han pasado por req.
+    console.log('params: ', req.params);
+
+    console.log('query: ', req.query);
+
+    res.send('respond with a resource');
+});
+
+// Peticion post con body
+router.post('/:id', function(req,res) {
+    console.log('body: ', req.body);
+
+    // Hay que devolver la peticion ya que sino el servidor se queda esperando
+    res.send('Codigo el body');
+});
+
 module.exports = router;
