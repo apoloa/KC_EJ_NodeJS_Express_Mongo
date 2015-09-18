7a02c7d7eb825642be476329668456bab07b6d23
Adrian Polo <a.whole.dev@gmail.com>
2015-09-18 21:00:12 +0200

    Added example from hoisting in javascript

---
+++ 00_hoisting/hoisting.js
@@ -0,0 +1,11 @@
+"use strict";
+
+var valor = 'hola';
+// Lo que hace Javascript es subir hacia arriba
+function pinta(){
+    var valor;
+    console.log(valor);
+    valor = 'adios';
+}
+
+pinta();
