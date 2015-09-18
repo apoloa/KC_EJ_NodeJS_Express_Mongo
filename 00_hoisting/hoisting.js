db34b191136f7bc73963f712063d21f45b9e4255
Adrian Polo <a.whole.dev@gmail.com>
2015-09-18 22:42:53 +0200

    Modificado el hoisting para hacer más entendible

--- 00_hoisting/hoisting.js
+++ 00_hoisting/hoisting.js
@@ -1,8 +1,14 @@
 "use strict";
 
 var valor = 'hola';
-// Lo que hace Javascript es subir hacia arriba
+// Lo que hace Javascript es subir hacia arriba la declaración de la variable, en este caso podemos ver como queda la
+// funcion pinta con el hoisting en pintaHoisting
 function pinta(){
+    console.log(valor);
+    var valor = 'adios';
+}
+
+funcion pintaHoisting(){
     var valor;
     console.log(valor);
     valor = 'adios';
