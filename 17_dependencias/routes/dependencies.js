7cfc84d95e2ab45845c4e3671673dd5178818abe
Adrian Polo <a.whole.dev@gmail.com>
2015-09-21 20:57:10 +0200

    Modificado para que se puedan controlar los errores y salgan de forma descriptiva en la web

--- 17_dependencias/routes/dependencies.js
+++ 17_dependencias/routes/dependencies.js
@@ -5,9 +5,15 @@ var router = express.Router();
 
 var readDependencies = require('../lib/readDependencies');
 
-router.get('/', function (req, res) {
+router.get('/', function (req, res, next) {
     readDependencies(function(err, data){
-        console.log(err,data);
+        // controlamos el error
+        if(err){
+            console.log(err);
+            return next(err);
+            // Acordarse de poner el return sino que seguiria el res.render de abajo. Entonces saldria error por
+            // intentar responder 2 veces 
+        }
         res.render('dependencies', {data: data});
     });
 });
